2b9d2cffb6e9f26a4c28e4a927999ed4a65d5246
sk1090 <156985260+sk1090@users.noreply.github.com>
2024-05-03 10:44:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+//global var currentfocus represents which game icon is selected/will be selected next
+let currentfocus = 0;
+(function() {
+
+  //adds an event listener for the arrow and tab keys
+  window.addEventListener("keydown", pressArrow);
+
+
+  function pressArrow(event) {
+  //  alert("hi");
+
+    //move current focus to the next element for right arrow, up arrows and tabs,
+    if ((event.keyCode == 9 || event.code === "ArrowRight"||event.code === "ArrowUp")) {
+        currentfocus++;
+        currentfocus = (currentfocus)%3;
+    }
+    //move current focus to the previous element for down and left arrows
+    else if ((event.code === "ArrowLeft"||event.code === "ArrowDown")) {
+          currentfocus--;
+          if(currentfocus<0)
+          {
+            currentfocus = currentfocus+3;
+          }
+          currentfocus = (currentfocus)%3;
+    }
+      //doesn't actually move the focus for tabs because that's already implemented
+    if(event.keyCode == 9)
+    {
+      return;
+    }
+    if(currentfocus<0)
+    {
+      currentfocus = currentfocus+3;
+    }
+
+    //focuses onto whichever element is the new currentfocus
+    if(currentfocus == 1)
+    {
+      document.querySelector("[tabindex='1']").focus();
+    }else if(currentfocus == 2)
+    {
+      document.querySelector("[tabindex='2']").focus();
+    }else{
+      document.querySelector("[tabindex='3']").focus();
+    }
+
+  }
+
+
+
+})();
